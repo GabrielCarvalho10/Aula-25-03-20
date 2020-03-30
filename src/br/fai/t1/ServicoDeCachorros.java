9e3decf7304e0e138b9a90664fbb3f76da4714ab
Your Name <GabrielCarvalho10>
2020-03-30 18:01:07 -0300

    5 commit

--- src/br/fai/t1/ServicoDeCachorros.java
+++ src/br/fai/t1/ServicoDeCachorros.java
@@ -1,12 +1,15 @@
 package br.fai.t1;
 
+///class ServicoDeCachorros implements Runnable serve para que possa usar o metodo run para usar thread.
 public class ServicoDeCachorros implements Runnable{
+	///Metodo que vai utilizar o thread e vai printar valores de 0 a 100 utilizando o for.
 	public void run() {
 		for (int i = 0; i < 100; i++) {
+			///plintar os numeros 
 			System.out.println("Id do serviço: " + id + " | valor: " + i);
 		}
 	}
-
+	///Uma variavel privada e de numero inteiro 
 	private int id;
 
 	public int getId() {
